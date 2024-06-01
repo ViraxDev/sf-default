78ef574488eba276b435faed4a34f65aba1c3ce1
ViraxDev <fartadji@gmail.com>
2024-06-01 20:37:05 +0200

    fix

--- assets/login.js
+++ assets/login.js
@@ -7,24 +7,19 @@ import('./theme/simplebar/simplebar.min.css');
 import('./theme/css/theme.css');
 import('./theme/css/user-rtl.css');
 import('./theme/css/user.css');
-
 import('@popperjs/core');
-
 import { Toast } from 'bootstrap';
-window.Toast = Toast;
-
 import AnchorJS from 'anchor-js';
-window.AnchorJS = AnchorJS;
-
 import is from 'is_js';
-window.is = is;
-
 import('./theme/vendors/fontawesome/all.min.js');
 import _ from 'lodash';
-window._ = _;
-
 import('./theme/vendors/list.js/list.min.js');
 import('./theme/theme.js');
+
+window.Toast = Toast;
+window.AnchorJS = AnchorJS;
+window.is = is;
+window._ = _;
 
 var isRTL = JSON.parse(localStorage.getItem('isRTL'));
 if (isRTL) {
