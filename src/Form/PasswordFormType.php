174fa8b390572170a8a7633722fc6f534877a97e
ViraxDev <fartadji@gmail.com>
2024-06-07 17:23:10 +0200

    feat: Refacto template email

---
+++ src/Form/PasswordFormType.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
+
+final class PasswordFormType extends AbstractType
+{
+    public function __construct(
+        #[Autowire(env: 'APP_ENV')]
+        private readonly string $env
+    ) {}
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $passwordConstraint = [new NotBlank([
+            'message' => 'Veuillez entrer un mot de passe',
+        ])];
+
+        if (!in_array($this->env, ['test', 'dev'])) {
+            array_push($passwordConstraint, new PasswordStrength(), new NotCompromisedPassword());
+        }
+
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => ['constraints' => $passwordConstraint],
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['inherit_data' => true]);
+    }
+}
